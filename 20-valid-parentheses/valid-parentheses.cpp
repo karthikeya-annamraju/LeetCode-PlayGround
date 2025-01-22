3aedcd0eb4d56f86008fd4c10bd1c2146afd6f85
Karthikeya Annamraju <86823119+karthikeya-annamraju@users.noreply.github.com>
2025-01-22 21:30:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (32.03%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (auto i: s) {
+            if (i == '(' or i == '[' or i == '{') {
+                st.push(i);
+            }
+            if (i == ')') {
+                if (!st.empty() && st.top() == '(') {
+                    st.pop();
+                }
+                else if (st.empty() or st.top() != '(') return false;
+            }
+
+            if (i == ']') {
+                if (!st.empty() && st.top() == '[') {
+                    st.pop();
+                }
+                else if (st.empty() or st.top() != '[') return false;
+            }
+
+            if (i == '}') {
+                if (!st.empty() && st.top() == '{') {
+                    st.pop();
+                }
+                else if (st.empty() or st.top() != '{') return false;
+            }
+        }
+        if (!st.empty())return false;
+        else return true;
+    }
+};
