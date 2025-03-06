cc4622a56d5abc198b5849fe6e3aa6e36a93350d
Karthikeya Annamraju <86823119+karthikeya-annamraju@users.noreply.github.com>
2025-03-06 10:36:27 +0530

    Time: 10 ms (65.26%) | Memory: 32.8 MB (5.79%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
         unordered_map<int, int>mp;
+        int repeated = 0;
         for (auto i: grid) {
             for (auto j: i) {
                 mp[j]++;
+                if (mp[j] == 2) repeated = j;
             }
         }
-        int repeated = 0;
-        for (auto it = mp.begin(); it != mp.end(); it++) {
-            if (it->second == 2) {
-                repeated = it->first;
-                break;
-            }
-        }
+        // for (auto it = mp.begin(); it != mp.end(); it++) {
+        //     if (it->second == 2) {
+        //         repeated = it->first;
+        //         break;
+        //     }
+        // }
         int n = grid.size();
-        int lim = n*n;
         int sum = 0;
-        int actualSum = (lim*(lim+1))/2;
+        int actualSum = ((n*n)*((n*n)+1))/2;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 sum+=grid[i][j];
