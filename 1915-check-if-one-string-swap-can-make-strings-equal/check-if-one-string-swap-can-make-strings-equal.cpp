d12a79d5c2698c8b4c74245fd187b5680ca5a4cc
Karthikeya Annamraju <86823119+karthikeya-annamraju@users.noreply.github.com>
2025-03-07 15:05:02 +0530

    Time: 2 ms (12.02%) | Memory: 8.6 MB (16.88%) - LeetSync

---
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        if (s1.size() != s2.size()) return false;
+        if (s1 == s2) return true;
+        vector<int>indices;
+        int cnt = 0;
+        for (int i = 0; i < s2.size(); i++) {
+            if (s1[i] != s2[i]) {
+                indices.push_back(i);
+                cnt++;
+            }
+        }
+        if (cnt != 2) return false;
+        cout << indices[0] << indices[1] << endl;
+        swap(s2[indices[0]], s2[indices[1]]);
+        cout << s1 << endl << s2;
+        return s1 == s2;
+    }
+};
