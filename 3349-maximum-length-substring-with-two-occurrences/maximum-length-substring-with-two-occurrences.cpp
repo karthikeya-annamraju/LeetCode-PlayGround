10f7b5d88211531052a3f3ab2109251e93d35764
Karthikeya Annamraju <86823119+karthikeya-annamraju@users.noreply.github.com>
2025-03-11 11:59:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (50.20%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
@@ -7,7 +7,7 @@ public:
             mp[s[j]]++;
             while (mp[s[j]] > 2) {
                 mp[s[i]]--;
-                if (mp[s[i]] == 0) mp.erase(s[i]);
+                // if (mp[s[i]] == 0) mp.erase(s[i]);
                 i++;
             }
             ans = max(ans, (j-i)+1);
