d81314ecf828f27027ecb57eb10f5e5d1f6ecfad
Sai Karthik Annamraju <86823119+karthikeya-annamraju@users.noreply.github.com>
2026-07-18 18:46:29 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (83.18%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int snowBallSize = 0;
+        int j = 0;
         for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == 0) {
-                snowBallSize++;
-            }
-            else if (snowBallSize > 0) {
-                int temp = nums[i];
-                nums[i] = 0;
-                nums[i-snowBallSize] = temp;
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
             }
         }
     }
