1b70c25d45ab290fc0b91b410da7dd2cb85aff6e
Sai Karthik Annamraju <86823119+karthikeya-annamraju@users.noreply.github.com>
2026-07-18 18:48:21 +0530

    Time: 3 ms (14.65%) | Memory: 24 MB (19.41%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        // this variable keeps track of the left most zero element that can be swapped with a non zero
         int j = 0;
+        // whenever we encounter a non-zero, we just swap it with the left most zero in the array in place
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != 0) {
                 swap(nums[i], nums[j]);
